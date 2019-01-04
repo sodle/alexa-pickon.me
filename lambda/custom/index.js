a7d5b5f6c7815f45ac11949d81a0dc6c8c555778
Scott Odle <scott@sjodle.com>
2019-01-04 13:30:28 -0700

    hello intent

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -8,12 +8,11 @@ const LaunchRequestHandler = {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
   handle(handlerInput) {
-    const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+    const speechText = 'From what period?';
 
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
       .getResponse();
   },
 };
